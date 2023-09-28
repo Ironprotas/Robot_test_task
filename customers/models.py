fdd60b0f6e6d6b8a2363a2825cf150c462f2e619
Ironprotas <ironprotas@gmail.com>
2023-09-28 18:57:16 +0300

    test

--- customers/models.py
+++ customers/models.py
@@ -1,5 +1,27 @@
 from django.db import models
+from django.core import mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from robots.models import  Robot
 
 
 class Customer(models.Model):
     email = models.CharField(max_length=255,blank=False, null=False)
+
+class RobotAvailability(models.Model):
+    robot = models.ForeignKey(Robot, on_delete=models.CASCADE)
+    available = models.BooleanField(default=False)
+    queued_for_notification = models.BooleanField(default=False)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def notify_customer(self):
+        if self.available and self.customer:
+            subject = 'Робот доступен в наличии'
+            message = render_to_string('availability_notification_email.html', {
+                'robot_model': self.robot.model,
+                'robot_version': self.robot.version,
+            })
+            plain_message = strip_tags(message)
+            from_email = 'noreply@example.com'
+            recipient_list = [self.customer.email]
+            mail.send_mail(subject, plain_message, from_email, recipient_list)
--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Robot, RobotAvailability, Customer
 
-# Create your views here.
+@csrf_exempt
+def check_robot_availability(request):
+    if request.method == 'GET':
+        model = request.GET.get('model')
+        version = request.GET.get('version')
+
+        try:
+            robot = Robot.objects.get(model=model, version=version)
+
+            queued_notifications = RobotAvailability.objects.filter(robot=robot, queued_for_notification=True)
+            for notification in queued_notifications:
+                notification.queued_for_notification = False
+                notification.save()
+                notification.notify_customer()
+
+            return JsonResponse({'message': 'Robot available'}, status=200)
+
+        except Robot.DoesNotExist:
+            customer_email = request.GET.get('customer')
+            customer, created = Customer.objects.get_or_create(email=customer_email)
+            robot_availability = RobotAvailability(robot=None, available=False, queued_for_notification=True,
+                                                   customer=customer)
+            robot_availability.save()
+
+            return JsonResponse({'message': 'Robot not available, notification queued'}, status=200)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
+
